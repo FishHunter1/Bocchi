84b197cb42c7172a6f6d761b6a2195974a4619cc
FishHunter1 <102452694+FishHunter1@users.noreply.github.com>
2023-04-26 15:52:33 -0500

    ae
    ae

---
+++ app/src/main/java/com/example/bocchi/ShowDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.bocchi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_detail);
+    }
+}
