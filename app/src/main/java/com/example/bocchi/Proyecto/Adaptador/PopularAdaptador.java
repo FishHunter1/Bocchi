c09c250f9b5f66924bb4ddf6d01e300531538225
abaza2022 <104527120+abaza2022@users.noreply.github.com>
2023-05-03 16:03:45 -0500

    awa
    awa

--- app/src/main/java/com/example/bocchi/Proyecto/Adaptador/PopularAdaptador.java
+++ app/src/main/java/com/example/bocchi/Proyecto/Adaptador/PopularAdaptador.java
@@ -7,8 +7,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -19,71 +17,46 @@ import com.example.bocchi.R;
 import java.util.ArrayList;
 
 public class PopularAdaptador extends RecyclerView.Adapter<PopularAdaptador.ViewHolder> {
-    ArrayList<FoodDomain>categoryFood;
+    ArrayList<FoodDomain> PopularFood;
 
     public PopularAdaptador(ArrayList<CategoryDomain> categoryDomains) {
-        this.categoryFood = categoryFood;
+        this.PopularFood = PopularFood;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category,parent,false);
+        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_popular,parent,false);
         return new ViewHolder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PopularAdaptador.ViewHolder holder, int position) {
 
-        holder.categoryName.setText(categoryFood.get(position).getTitle());
-        String picUrl="";
-        switch (position){
-            case 0:{
-                picUrl="cat_1";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(),R.drawable.cat_background1));
-                break;
-            }
-            case 1:{
-                picUrl="cat_2";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(),R.drawable.cat_background2));
-                break;
-            }
-            case 2:{
-                picUrl="cat_3";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(),R.drawable.cat_background3));
-                break;
-            }
-            case 3:{
-                picUrl="cat_4";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(),R.drawable.cat_background4));
-                break;
-            }
-            case 4:{
-                picUrl="cat_5";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(),R.drawable.cat_background5));
-                break;
-            }
-        }
-        int drawableResourceId=holder.itemView.getContext().getResources().getIdentifier(picUrl,"drawable",holder.itemView.getContext().getPackageName());
+        holder.title.setText(PopularFood.get(position).getTitle());
+        holder.fee.setText(String.valueOf(PopularFood.get(position).getFee()));
+
+        int drawableResourceId=holder.itemView.getContext().getResources().getIdentifier(PopularFood.get(position).getPic(),"drawable",holder.itemView.getContext().getPackageName());
 
         Glide.with(holder.itemView.getContext())
                 .load(drawableResourceId)
-                .into(holder.categoryPic);
+                .into(holder.pic);
     }
 
     @Override
     public int getItemCount() {
-        return categoryFood.size();
+        return PopularFood.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView categoryName;
-        ImageView categoryPic;
-        ConstraintLayout mainLayout;
+        TextView title,fee;
+        TextView addBtn;
+        ImageView pic;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            categoryName=itemView.findViewById(R.id.categoryName);
-            categoryPic=itemView.findViewById(R.id.categoryPic);
-            mainLayout=itemView.findViewById(R.id.mainLayout);
+            title=itemView.findViewById(R.id.title);
+            fee=itemView.findViewById(R.id.fee);
+            pic=itemView.findViewById(R.id.pic);
+            addBtn=itemView.findViewById(R.id.addBtn);
         }
     }
 }
