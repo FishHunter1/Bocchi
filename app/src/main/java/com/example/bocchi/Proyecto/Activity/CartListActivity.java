28f2cab175d1fc2a63fe7303ceb32b6e516ddf35
abaza2022 <104527120+abaza2022@users.noreply.github.com>
2023-05-10 15:54:52 -0500

    cosa

---
+++ app/src/main/java/com/example/bocchi/Proyecto/Activity/CartListActivity.java
@@ -0,0 +1,16 @@
+package com.example.bocchi.Proyecto.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bocchi.R;
+
+public class CartListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_list);
+    }
+}
